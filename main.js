d5bdffa7f7d5bb5442edab7de24eea8effe1e102
zezsk <112283266+zezsk@users.noreply.github.com>
2023-10-12 12:34:00 -0400

    first commit

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
 console.log("hello world");
 console.log(1*9/3 + 2);
 
-let i = 1;
-
-do {
-  alert('Hello, The current index/num is: ' + i);
-  document.write('<li>Hello, The current index/num is: ' + i + '</li>');
-  console.log('Hello, The current index/num is: ' + i);
-  i++;
-}
-while (i <= 5); 
